88cdc76190def20e34139203566d5e379d884700
Artem <arimgenov.2016@mail.ru>
2024-02-16 01:08:49 +0300

    версия рабочих функций

--- problem_1.cpp
+++ problem_1.cpp
@@ -3,17 +3,46 @@
 #include <random>
 
 
-int main() {
-    int n;
-    std::cin >> n;
-    int array[n];
+void search_ln(int n, int arr[], int x, int &y) {
+    int l = 0, r = n-1, s;
+    while (r >= l) {
+        s = (l + r)/2;
+        if (arr[s] == x) {
+            y = s;
+            break;
+        } else if (arr[s] > x) {
+            r = s - 1;
+        } else {
+            l = s + 1;
+        }
+    }
+}
+
+void search_lin(int n, int arr[], int x, int &y) {
     for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            y = i;
+            break;
+        }
+    }
+}
+
+
+int main() {
+    int n, x, y; // n - число элементов массива, x - искомый элемент, y - номер элемента (-1 если его нет)
+    std::cin >> n >> x;
+    int array[n];
+    for (int i = 0; i < n; i++) { // вписываю в массив значения
         std::cin >> array[i];
     }
 
-    for (int i = 0; i < n; i++) {
-        std::cout << array[i] << " ";
-    }
+    y = -1;
+    search_lin(n, array, x, y);
+    std::cout << y << "\n";
+
+    y = -1;
+    search_ln(n, array, x, y);
+    std::cout << y << "\n";
 
     return 0;
 }
