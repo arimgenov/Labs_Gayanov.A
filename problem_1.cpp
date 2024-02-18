d081b3d69e6dac66541062c3c44accae517a0316
Artem <arimgenov.2016@mail.ru>
2024-02-19 00:15:58 +0300

    0.6

--- problem_1.cpp
+++ problem_1.cpp
@@ -6,15 +6,15 @@
 
 
 int main() {
-    int array[200000];
-    for (int i = 0; i < 200000; i++) {
+    int array[300000];
+    for (int i = 0; i < 300000; i++) {
         array[i] = i+1;
     }
 
     std::ofstream out;
     out.open("points.txt");
     
-    
+    std::cout << array[0] << " Uspex\n";
 
     out.close();
     return 0;
